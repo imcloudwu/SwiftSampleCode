4294d86e698efb194e7367a8f644da8e173f4eae
Cloud <imcloudwu@gmail.com>
2014-09-25 15:23:53 +0800

    CollectionView

---
+++ CollectionView.swift
@@ -0,0 +1,76 @@
+//
+//  SecondViewController.swift
+//  tab
+//
+//  Created by Cloud on 9/24/14.
+//  Copyright (c) 2014 Cloud. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource {
+
+    @IBOutlet var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as CollectionViewCell
+        cell.backgroundColor = UIColor.whiteColor()
+        cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
+        //cell.imageView?.image = UIImage(named: "circle")
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!)
+    {
+        var cell = collectionView.cellForItemAtIndexPath(indexPath)
+        
+        println("\(indexPath.section):\(indexPath.row) is clicked")
+    }
+}
+
+class CollectionViewCell: UICollectionViewCell {
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    let textLabel: UILabel!
+    let imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 16, width: frame.size.width, height: frame.size.height*2/3))
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        contentView.addSubview(imageView)
+        
+        let textFrame = CGRect(x: 0, y: 32, width: frame.size.width, height: frame.size.height/3)
+        textLabel = UILabel(frame: textFrame)
+        textLabel.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
+        textLabel.textAlignment = .Center
+        contentView.addSubview(textLabel)
+    }
+}
+
